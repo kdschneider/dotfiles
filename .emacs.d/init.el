caf963771539a9ec556d61f541a10235abf095fb
kdschneider <ks@konstantin-schneider.eu>
2022-02-12 20:57:10 +0100

    Becoming EVIL...

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,13 +18,25 @@
 
 (set-face-attribute 'default nil :family "FiraCode Nerd Font Mono" :height 180)
 
-;; Theme
-(load-theme 'wombat)
+;; Transparency
+(set-frame-parameter (selected-frame) 'alpha '(85 85))
 
 ;; Escape quit
-(global-set-key (kbd "<escape>") 'keyboard-escape-qui
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 ;; Initialize package sources
 (require 'package)
+
+;; Editor
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
 			 ("elpa" . "https://elpa.gnu.org/packages/")))
@@ -51,8 +63,86 @@
   :config
   (ivy-mode 1))
 
+;; Counsel
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
 
-;; Modeline
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; Icons
+(use-package all-the-icons)
+
+;; Doom modeline
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
+
+;; Doom Themes
+(use-package doom-themes
+  :init (load-theme 'doom-dracula t))
+
+;; Rainbow delims
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Which key
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+;; Get help..
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; Becoming evil
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+  
+;; General (better keybindings)
+(use-package general)
+
+;; Hydra
+(use-package hydra)
+
+    ;; Custom-set variables (wtf is this garbaje??)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(hydra which-key use-package rainbow-delimiters ivy-rich helpful general evil-collection doom-themes doom-modeline counsel command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
